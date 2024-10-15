cfa3e755d56333b15d4e007c16a904d51199b91d
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-10-15 23:48:00 +0600

    post part done

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express= require('express');
 const app = express();
+const bodyParser=require('body-parser')
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.json({message: "Welcome to Rifah's Practice app!" });
+})
+
+let users= []
+let lastId= 0
+
+app.post('/users', (req,res) =>{
+    const user= req.body
+    user.id= ++lastId;
+    users.push(user)
+    res.status(201).json(user)
 })
 
 const port= 5000;
