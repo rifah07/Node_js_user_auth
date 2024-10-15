84fd1d4cc2c7a7ca32e003b15e3a04fba9aaa5a6
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-10-15 23:25:10 +0600

    connection esrtablished

---
+++ server.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({message: "Welcome to Rifah's Practice app!" });
+})
+
+const port= 5000;
+
+app.listen(port, ()=>{
+    console.log(`Server running at http://localhost:${port}`);
+});
