bff8a02df7ded15cf92732142486c0a71dd39b54
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-10-16 00:03:11 +0600

    Get for one id implemented

--- server.js
+++ server.js
@@ -22,6 +22,18 @@ app.get('/users',(req,res) =>{
     res.json(users)
 })
 
+app.get('/user/:id', (req, res)=>{
+    const id= parseInt(req.params.id)
+    const user= users.find(u => u.id === id)
+
+    if(user){
+        res.json(user)
+    }
+
+    else{
+        res.status(404).json({message: 'User not found!'})
+    }
+})
 
 const port= 5000;
 
